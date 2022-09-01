f86ac8dc7793842b8dbbf2036d834b7257d2a775
이강은 <rkddms123456@gmail.com>
2022-09-01 20:15:36 +0900

    다시 1일1문제 풀이 도전..!

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args){
+        Programmers02 a = new Programmers02();
+        a.solution(10);
+        System.out.println(a.solution(20));
+    }
+}
