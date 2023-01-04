6e69bbbb2aa325960d8e4e241dcc1d52514e4ea1
이강은 <rkddms123456@gmail.com>
2023-01-05 00:15:35 +0900

    백준 2750

--- src/baekjoon/baek_2750.java
+++ src/baekjoon/baek_2750.java
@@ -1,6 +1,8 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class baek_2750 {
 
@@ -17,6 +19,23 @@ public class baek_2750 {
 
         for (int i = 0; i < arr.length; i++) {
 
+            for (int j = 0; j < arr.length; j++) {
+
+                int one = arr[i];
+                int two = arr[j];
+
+                if (two > one) {
+                    arr[i] = two;
+                    arr[j] = one;
+                }
+
+            }
+
+        }
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
     }
 }
