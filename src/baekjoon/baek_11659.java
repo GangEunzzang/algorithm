20571f19dee479ea6ee002acf77310c8c7d4d363
이강은 <rkddms123456@gmail.com>
2023-01-08 12:30:04 +0900

    백준 11659

--- src/baekjoon/baek_11659.java
+++ src/baekjoon/baek_11659.java
@@ -1,37 +1,56 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class baek_11659 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int arr[] = new int[N];
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+
+        st = new StringTokenizer(br.readLine());
+        int[] sumArrNum = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            sumArrNum[i] = sumArrNum[i-1] + Integer.parseInt(st.nextToken());
         }
+
 
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0 ; i < M; i++) {
 
-            int indexSum = startIndexEndIndexSum(arr, sc.nextInt(), sc.nextInt());
+            st = new StringTokenizer(br.readLine());
 
-            sb.append(indexSum).append("\n");
+            int ii = Integer.parseInt(st.nextToken());
+            int jj = Integer.parseInt(st.nextToken());
+
+            sb.append(sumArrNum[jj] - sumArrNum[ii-1]).append("\n");
         }
 
         System.out.println(sb);
 
+        br.close();
     }
 
     public static int startIndexEndIndexSum(int[] arr, int start, int end) {
 
+        System.out.println("start = " + start);
+        System.out.println("end = " + end);
         int sum = 0;
 
         for (int i = start-1; i < end; i++) {
