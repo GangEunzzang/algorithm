ab5464db6f682244de73747e125d2f2b2b3c5266
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-02-12 18:57:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 21876 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] visited = new int[1000000];
+
+    static int N,K;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+
+        bfs(N);
+
+        System.out.println(visited[K]-1);
+
+    }
+
+    private static void bfs(int n) {
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+
+        visited[n] = 1;
+
+        while (!q.isEmpty()) {
+
+            Integer poll = q.poll();
+
+            int up = poll + 1;
+            int down = poll -1;
+            int jump = poll * 2;
+            
+            
+            if (up <= 100000 && visited[up] == 0) {
+                q.add(up);
+                visited[up] = visited[poll] + 1;
+            }
+
+            if (down >= 0 && visited[down] == 0) {
+                q.add(down);
+                visited[down] = visited[poll] + 1;
+            }
+
+            if (jump <= 100000 && visited[jump] == 0) {
+                q.add(jump);
+                visited[jump] = visited[poll] + 1;
+            }
+
+            if (up == K || down == K || jump == K) {
+                break;
+            }
+
+        }
+
+    }
+
+
+}
