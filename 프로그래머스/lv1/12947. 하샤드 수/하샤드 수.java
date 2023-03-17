7bcba00886dddaff349ebe7632b38733ef07ac27
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-03-17 23:16:03 +0900

    [level 1] Title: 하샤드 수, Time: 0.10 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+
+        String [] arr = String.valueOf(x).split("");
+        
+        int sum = 0;
+        
+        for (int i = 0; i < arr.length; i++){
+            sum += Integer.parseInt(arr[i]);
+        }
+        
+        
+        return x % sum == 0 ? true : false;
+    }
+}
