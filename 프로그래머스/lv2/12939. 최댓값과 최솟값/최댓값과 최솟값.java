1ce3dfe141b5210fabbd40b5462d2ba292841415
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-03-22 16:00:11 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.14 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        
+        String[] arr = s.split(" ");
+        List<Integer> list = new ArrayList<>();
+        
+        for (String num : arr) {
+            list.add(Integer.parseInt(num));
+        }
+        
+        Collections.sort(list);
+        
+        String answer = list.get(0) + " " +  list.get(list.size()-1);
+        
+        return answer;
+    }
+}
