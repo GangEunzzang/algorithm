33e62d23843fafffc1d27edb8417a1f024236ba1
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-03-28 23:44:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.64 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,34 +1,73 @@
-import java.util.*;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
 
-    private int[][] map;
-    private boolean[][] visited;
+    private static boolean[][] visited;
+    private static int[][] map;
 
-    private int[] moveOfN = {0, 0, -1, 1};
-    private int[] moveOfM = {1, -1, 0, 0};
+    private static int width;
+
+    private static int height;
+
+    private static int[] moveOfN = {0, 0, 1, -1};
+    private static int[] moveOfM = {1, -1, 0, 0};
 
     public int solution(int[][] maps) {
+
+        width = maps.length - 1;
+        height = maps[0].length - 1;
 
         map = new int[maps.length][maps[0].length];
         visited = new boolean[maps.length][maps[0].length];
 
         for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps[0].length; j++) {
+            for (int j = 0; j < maps[i].length; j++) {
                 if (maps[i][j] == 0) {
                     visited[i][j] = true;
                 }
             }
         }
 
-        bfs(0, 0);
+        bfs(0, 0, 0);
 
-        return map[map.length-1][map[0].length-1] == 0 ? -1 : map[map.length-1][map[0].length-1];
+
+        return map[width][height] == 0 ? -1 : map[width][height];
     }
 
-    private void bfs(int n, int m) {
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(n, m));
-        map[n][m] = 1;
+    private static void dfs(int n, int m, int count) {
+
+        if (visited[n][m]) {
+            return;
+        }
+
+        visited[n][m] = true;
+
+        if (map[n][m] <= count + 1) {
+            map[n][m] = count + 1;
+        }
+
+        for (int i = 0; i < 4; i++) {
+
+            int arrivalOfN = n + moveOfN[i];
+            int arrivalOfM = m + moveOfM[i];
+
+            if (arrivalOfN < 0 || arrivalOfM < 0 || arrivalOfN > width || arrivalOfM > height) {
+                continue;
+            }
+
+            if (visited[arrivalOfN][arrivalOfM]) {
+                continue;
+            }
+
+            dfs(arrivalOfN, arrivalOfM, map[n][m]);
+        }
+    }
+
+    private static void bfs(int n, int m, int count) {
+        Queue<Pair> queue = new LinkedList();
+        queue.add(new Pair(n,m));
 
         while (!queue.isEmpty()) {
 
@@ -36,41 +75,40 @@ class Solution {
 
             for (int i = 0; i < 4; i++) {
 
-                int movingOfN = poll.getX() + moveOfN[i];
-                int movingOfM = poll.getY() + moveOfM[i];
+                int arrivalOfN = poll.getX() + moveOfN[i];
+                int arrivalOfM = poll.getY() + moveOfM[i];
 
-                if (movingOfN < 0 || movingOfM < 0 || movingOfN >= map.length || movingOfM >= map[0].length) {
+                if (arrivalOfN < 0 || arrivalOfM < 0 || arrivalOfN > width || arrivalOfM > height) {
                     continue;
                 }
 
-                if (visited[movingOfN][movingOfM]) {
+                if (visited[arrivalOfN][arrivalOfM]) {
                     continue;
                 }
 
-                queue.add(new Pair(movingOfN, movingOfM));
-                visited[movingOfN][movingOfM] = true;
-                map[movingOfN][movingOfM] = map[poll.getX()][poll.getY()] + 1;
+                queue.add(new Pair(arrivalOfN, arrivalOfM));
+                map[arrivalOfN][arrivalOfM] = map[poll.getX()][poll.getY()] + 1;
+                visited[arrivalOfN][arrivalOfM] = true;
             }
         }
     }
-
+    
     private static class Pair {
-
-        private int x;
-        private int y;
 
         Pair(int x, int y) {
             this.x = x;
             this.y = y;
         }
+        private int x;
+        private int y;
 
         public int getX() {
             return x;
         }
 
-        public int getY() {
+        public int getY(){
             return y;
         }
+
     }
 }
-    
