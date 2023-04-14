a388c4ae1721b1796841eb491756d532d35d3b17
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-04-14 13:33:20 +0900

    [level 2] Title: 주식가격, Time: 10.95 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] prices) {
+
+        int[] answer = new int[prices.length];
+        answer[prices.length - 1] = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            int count = 0;
+
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] < prices[i]) {
+                    count++;
+                    break;
+                }
+                count++;
+            }
+            answer[i] = count;
+        }
+        return answer;
+    }
+}
