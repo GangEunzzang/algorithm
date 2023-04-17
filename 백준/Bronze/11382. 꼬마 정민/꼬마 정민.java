b7e49b78dd38b0d3b185810a8cfde7bbc3d94a21
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-04-17 16:22:12 +0900

    [Bronze V] Title: 꼬마 정민, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.Year;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println(Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken()));
+
+    }
+
+}
