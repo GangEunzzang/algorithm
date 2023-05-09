9b90c3709c73595713db3775829765050be28c32
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-05-09 16:58:51 +0900

    [Gold V] Title: 센서, Time: 220 ms, Memory: 16604 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        List<Integer> list = new ArrayList<>();
+
+
+        for (int i = 1; i < N; i++) {
+            int num = arr[i] - arr[i - 1];
+            list.add(num);
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+        int result = 0;
+
+        for (int i = K-1; i < N - 1; i++) {
+            result += list.get(i);
+        }
+
+        System.out.println(result);
+
+    }
+}
