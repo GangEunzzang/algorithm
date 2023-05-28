0da35dffc7c195289bc88bdbf429d5d107512f4d
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-05-28 19:24:31 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 148 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,36 +1,35 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Main {
-
-    private static int[][] gearArr;
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int[] arr = new int[A];
-        int[] dp = new int[A];
+        int[] arr = new int[N];
+        int[] dp = new int[N];
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < A; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        
-        for (int i = 0; i < A; i++) {
+        for (int i = 0; i < N; i++) {
             dp[i] = arr[i];
             for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j] && dp[j] + arr[i] > dp[i]) {
-                    dp[i] = dp[j] + arr[i];
-                }
+                if (arr[j] < arr[i])
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
             }
         }
-        
 
-        System.out.println(Arrays.stream(dp).max().orElse(0));
+        int max = 0;
+        for (int i = 0; i < N; i++) max = Math.max(max, dp[i]);
+        System.out.println(max);
+
+
+
     }
+
 }
