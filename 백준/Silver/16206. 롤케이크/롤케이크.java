7354d4e00a40409e9eb318a746a56e5ec5296058
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-05-30 22:07:53 +0900

    [Silver I] Title: 롤케이크, Time: 140 ms, Memory: 14780 KB -BaekjoonHub

---
+++ 백준/Silver/16206. 롤케이크/롤케이크.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator
+                .comparingInt((Integer num) -> num % 10)
+                .thenComparingInt(num -> num));
+
+
+        int count = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num == 10) {
+                count++;
+                continue;
+            }
+            if (num < 10) continue;
+
+            pq.add(num);
+        }
+
+        while (M > 0 && !pq.isEmpty()) {
+            Integer poll = pq.poll();
+
+            if (poll - 10 == 10) {
+                count++;
+            }
+
+            if (poll - 10 > 10) {
+                pq.add(poll - 10);
+            }
+
+            count++;
+            M--;
+        }
+
+        System.out.println(count);
+    }
+}
