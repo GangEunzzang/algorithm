ea27e6a4574dbb2a94e4486a7da75c7292d78065
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-06-14 19:28:22 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 292 ms, Memory: 21208 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -5,6 +5,17 @@ import java.util.*;
 
 public class Main {
 
+    /**
+     * 처음 시작하는 구름 위치는  (N, 1), (N, 2), (N-1, 1), (N-1, 2)
+     * 지정한 방향의 횟수만큼 이동 (%로 처리)
+     * 이동 후 비구름이 있는 애들은 물의 양 1씩 증가
+     * 구름이 모두 사라짐
+     * 물이 증가한 칸에서만 (list 로 보관) 대각선 방향으로 거리가 1인 칸에 물이 있는 바구니의 수만큼 (r,c)에 있는 바구니의 물의 양이 증가
+     * list에 보관된 애들을 제외한 물의 양이 2이상인 모든 칸에 구름이 생기고, 물의 양이 2가 줄어듦
+     * M번 반복
+     *
+     */
+    
     private static int N;
     private static int M;
     private static int direction;
@@ -58,33 +69,20 @@ public class Main {
 
         System.out.println(sum);
 
-        /**
-         * 처음 시작하는 구름 위치는  (N, 1), (N, 2), (N-1, 1), (N-1, 2)
-         * 지정한 방향의 횟수만큼 이동 (%로 처리)
-         * 이동 후 비구름이 있는 애들은 물의 양 1씩 증가
-         * 구름이 모두 사라짐
-         * 물이 증가한 칸에서만 (list 로 보관) 대각선 방향으로 거리가 1인 칸에 물이 있는 바구니의 수만큼 (r,c)에 있는 바구니의 물의 양이 증가
-         * list에 보관된 애들을 제외한 물의 양이 2이상인 모든 칸에 구름이 생기고, 물의 양이 2가 줄어듦
-         * M번 반복
-         *
-         */
+ 
 
     }
 
     private static void waterCopy() {
-        Queue<Node> q = new LinkedList<>(rainCloudList);
-
-        while (!q.isEmpty()) {
-            Node node = q.poll();
+        for (Node node : rainCloudList) {
 
             int sum = 0;
 
             for (int i = 0; i < 8; i++) {
                 if (i % 2 == 0) continue;
-
                 int nx = node.x + moveX[i];
                 int ny = node.y + moveY[i];
-
+                
                 if (nx >= 0 && ny >= 0 && nx <= N && ny <= N && arr[nx][ny] > 0) sum++;
             }
 
@@ -109,7 +107,7 @@ public class Main {
         for (Node node : rainCloudList) {
             int nx = node.x;
             int ny = node.y;
-            
+
             for (int i = 0; i < number; i++) {
                 nx = nx + moveX[direction - 1];
                 ny = ny + moveY[direction - 1];
