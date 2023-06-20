5a6e2e81ee5217951111ed21456156c696d2985d
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-06-20 16:28:48 +0900

    [level 2] Title: [3차] n진수 게임, Time: 2.28 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -16,38 +16,20 @@ class Solution {
 
         while (sb.length() < t) {
             String value = convert(n, idx);
-            
+
             for (int i = 0; i < value.length(); i++) {
                 if(talkCount++ == p && sb.length() < t) sb.append(value.charAt(i));
                 if(talkCount > m) talkCount = 1;
             }
-            
+
             idx++;
-            
+
         }
-        return sb.toString();
+        return sb.toString().toUpperCase();
     }
 
     private String convert(int radix, int num) {
-
-        switch (radix) {
-            case 2 : return Integer.toString(num, 2);
-            case 3 : return Integer.toString(num, 3);
-            case 4 : return Integer.toString(num, 4);
-            case 5 : return Integer.toString(num, 5);
-            case 6 : return Integer.toString(num, 6);
-            case 7 : return Integer.toString(num, 7);
-            case 8 : return Integer.toString(num, 8);
-            case 9 : return Integer.toString(num, 9);
-            case 10 : return Integer.toString(num, 10);
-            case 11 : return Integer.toString(num, 11).toUpperCase();
-            case 12 : return Integer.toString(num, 12).toUpperCase();
-            case 13 : return Integer.toString(num, 13).toUpperCase();
-            case 14 : return Integer.toString(num, 14).toUpperCase();
-            case 15 : return Integer.toString(num, 15).toUpperCase();
-            case 16 : return Integer.toString(num, 16).toUpperCase();
-        }
-
-        throw new IllegalArgumentException("Invalid Parameter");
+        String s = Integer.toString(num, radix);
+        return s;
     }
 }  
