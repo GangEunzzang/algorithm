6429febe8e0224d379f1502d84f32ada37c6386c
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-06-20 15:57:28 +0900

    [level 2] Title: [3차] n진수 게임, Time: 3.58 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public static void main(String[] args) {
+        new Solution().solution(2, 4, 2, 1);
+    }
+    // n -> 진법 (2,4,8,16)
+    // t -> 말해야하는 개수
+    // m -> 게임 참가 인원
+    // p -> 순서
+    public String solution(int n, int t, int m, int p) {
+
+        StringBuilder sb = new StringBuilder();
+        if(p == 1) sb.append(0);
+
+        int idx = 0;
+        int talkCount = 2;
+
+        while (sb.length() < t) {
+            idx++;
+            String value = getValue(n, idx);
+            for (int i = 0; i < value.length(); i++) {
+                char c = value.charAt(i);
+
+                if(talkCount++ == p && sb.length() < t) sb.append(c);
+                if(talkCount > m) talkCount = 1;
+            }
+
+        }
+        return sb.toString();
+    }
+
+    private String getValue(int n, int num) {
+
+        switch (n) {
+            case 2 : return Integer.toString(num, 2);
+            case 3 : return Integer.toString(num, 3);
+            case 4 : return Integer.toString(num, 4);
+            case 5 : return Integer.toString(num, 5);
+            case 6 : return Integer.toString(num, 6);
+            case 7 : return Integer.toString(num, 7);
+            case 8 : return Integer.toString(num, 8);
+            case 9 : return Integer.toString(num, 9);
+            case 10 : return Integer.toString(num, 10);
+            case 11 : return Integer.toString(num, 11).toUpperCase();
+            case 12 : return Integer.toString(num, 12).toUpperCase();
+            case 13 : return Integer.toString(num, 13).toUpperCase();
+            case 14 : return Integer.toString(num, 14).toUpperCase();
+            case 15 : return Integer.toString(num, 15).toUpperCase();
+            case 16 : return Integer.toString(num, 16).toUpperCase();
+        }
+
+        throw new IllegalArgumentException("Invalid Parameter");
+    }
+}  
