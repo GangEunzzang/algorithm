642f3c1b79290a80606e61c1732cb7414a795c27
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-06-29 14:55:46 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("     /~\\").append("\n");
+        sb.append("    ( oo|").append("\n");
+        sb.append("    _\\=/_").append("\n");
+        sb.append("   /  _  \\").append("\n");
+        sb.append("  //|/.\\|\\\\").append("\n");
+        sb.append(" ||  \\ /  ||").append("\n");
+        sb.append("============").append("\n");
+        sb.append("|          |").append("\n");
+        sb.append("|          |").append("\n");
+        sb.append("|          |");
+
+
+        System.out.println(sb);
+
+    }
+}
