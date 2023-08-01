b3fcbbf7bd58c6f1650e7eb4ddbc3741fcb9d038
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-08-01 14:43:28 +0900

    [Silver II] Title: 상자넣기, Time: 156 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        int result = 0;
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(dp[i], result);
+        }
+
+        System.out.println(result);
+
+    }
+
+
+}
