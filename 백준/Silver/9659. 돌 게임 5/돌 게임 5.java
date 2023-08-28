816db385378bc709b017cfb9b02b3c666f83769b
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-08-28 21:32:59 +0900

    [Silver III] Title: 돌 게임 5, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+
+        System.out.println(N % 2 == 1 ? "SK" : "CY");
+    }
+}
+
+
