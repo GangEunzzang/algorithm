09fa1829f6baa5a85aae4d7dcd160b85e3082577
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-12-01 14:59:36 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1468 ms, Memory: 102140 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        String answer = Arrays.stream(arr).mapToObj(i -> i + " ").collect(Collectors.joining());
+
+        System.out.println(answer);
+    }
+
+
+}
