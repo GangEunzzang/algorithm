8ac0da62d6e1cb2143db6240ce785cd999c3d23d
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-12-01 14:41:48 +0900

    [Silver IV] Title: 알바생 강호, Time: 268 ms, Memory: 23996 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        Integer[] arr = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        long sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            int tip = arr[i] - i;
+            if(tip < 0) break;
+            sum += tip;
+        }
+
+        System.out.println(sum);
+    }
+
+
+}
