3bc60c3032d1df11e6205a25ee75eb382de6fc63
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-12-06 09:05:35 +0900

    [Gold V] Title: 수 고르기, Time: 356 ms, Memory: 27992 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = 0;
+
+        int result = Integer.MAX_VALUE;
+
+        while (start < N) {
+            int num = arr[start] - arr[end];
+
+            if (num < M) {
+                start++;
+                continue;
+            }
+
+            if (num == M) {
+                result = M;
+                break;
+            }
+            
+            result = Math.min(result, num);
+            end++;
+
+        }
+
+        System.out.println(result);
+
+
+    }
+
+
+}
