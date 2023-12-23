60bfe736b5ee92229ed860a2c61be8af38996cbb
이강은 <73535356+GangEunzzang@users.noreply.github.com>
2023-12-23 22:26:37 +0900

    [Gold V] Title: 감시 피하기, Time: 160 ms, Memory: 20376 KB -BaekjoonHub

---
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static char[][] arr;
+    static int N;
+    static boolean flag;
+    static int[] moveX = {-1, 1, 0, 0};
+    static int[] moveY = {0, 0, -1, 1};
+
+    public static void main(String args[]) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        arr = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = st.nextToken().charAt(0);
+            }
+        }
+
+        dfs(0);
+
+        System.out.println(flag ? "YES" : "NO");
+    }
+
+
+    static void dfs(int hurdleCount) {
+        if (flag) return;
+
+        if (hurdleCount == 3) {
+            check();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] != 'X') continue;
+                arr[i][j] = 'O';
+                dfs(hurdleCount + 1);
+                arr[i][j] = 'X';
+            }
+        }
+    }
+
+    static void check() {
+        Queue<Node> q = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 'T') {
+                    q.add(new Node(i, j));
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int idx = 1;
+                while (true) {
+                    int mx = node.x + moveX[i] * idx;
+                    int my = node.y + moveY[i] * idx++;
+                    if(mx < 0 || my < 0 || mx >= N || my >= N || arr[mx][my] == 'O') break;
+                    if (arr[mx][my] == 'S') return;
+                }
+            }
+        }
+
+        flag = true;
+    }
+
+    static class Node {
+        int x;
+        int y;
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
